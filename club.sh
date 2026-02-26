f317dab0d4a64870d3eac820ce145d13dd4f694e
oyebodeabi-a11y <oyebodeabi@gmail.com>
2026-02-26 13:36:24 +0000

    bash

--- club.sh
+++ club.sh
@@ -1,4 +1,5 @@
 club="liverpool"
 country="UnitedKingdom"
 fans="english fans"
-echo 
+echo "I love footballers that play for" $club "in" $country.
+echo The $fans "are very vociverous particularly the" $club "fans"

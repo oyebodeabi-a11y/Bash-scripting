eeee583a2374933e677e30b85c05965e787468f7
oyebodeabi-a11y <oyebodeabi@gmail.com>
2026-03-01 22:49:26 +0000

    addition

---
+++ restoration.sh
@@ -0,0 +1,18 @@
+# You are meant to create another script give it a suitable name with a .sh extension  
+# develop at the script that echo the following: please check through any word that 
+#appears twice take it out and form a variable in the echo2 below 
+# Echo2 
+# Yes I am a Christain  but at times my faith has a Christian  has been shaken by various 
+# challenges in my country of birth England. Yes Although England is still a good country 
+# and still a choice definition for many folks  but it largely now lives on its old glory. I hope 
+# one day the lord will bring back its lots glory
+
+# solution
+religion="christian"
+country="England"
+place="country"
+faith="glory"
+echo "Yes I am a" $religion "but at times my faith as a" $religion "has been shaken by various"
+echo "challenges in my" $place "of birth" $country. "Yes Although" $country "is still a good" $place
+echo "and still a choice definition for many folks  but it largely now lives on its old" $faith
+echo "one day the lord will bring back its lost" $faith

acf14a62ec1a0d2f0487cf497602149cda676ce9
oyebodeabi-a11y <oyebodeabi@gmail.com>
2026-03-01 22:33:42 +0000

    new file

---
+++ football.sh
@@ -0,0 +1,18 @@
+# You are meant to create a script call it football.sh  
+# develop at the script that echo the following: 
+# pls check through any word that appears twice take it out and form a variable 
+# in the echo1 below 
+#Echo1 
+# I love football and I love watching football particularly in England. 
+# Among  the United Kingdom countries, England is still number one when it comes to playing football. 
+# Although football is still team sport but is full of violent particularly 
+# when is played in England even though the love for the game is high 
+
+# solution
+feeling="love"
+game="football"
+country="England"
+
+echo "I" $feeling $game and "I" $feeling "watching" $game "particularly in" $country.
+echo "Although" $game "is still team sport but is full of violent particulary when play in" $country "even though the" $feeling 
+echo "for the game is high".
